44ae3ebf474ff3966087c21cf1ed55d4dfa8bfa4
shira dusowitz <sdusowitz@gmail.com>
2016-03-08 19:16:50 -0500

    fixed logic error

--- src/main/java/groupProject/twentyFortyEight/GameLogic.java
+++ src/main/java/groupProject/twentyFortyEight/GameLogic.java
@@ -12,7 +12,7 @@ public class GameLogic {
 	private Tile current, comparison;
 	private boolean merged;
 	private int score;
-	private int bestScore;
+	private final int bestScore;
 
 	public GameLogic() {
 		this.gameTiles = new Tile[4][4];
@@ -30,8 +30,7 @@ public class GameLogic {
 	public Tile addTile() {
 		availableSlots = emptySlots();
 		if (!availableSlots.isEmpty()) {
-			int index = (int) (Math.random() * availableSlots.size())
-					% availableSlots.size();
+			int index = (int) (Math.random() * availableSlots.size()) % availableSlots.size();
 			chosenSlot = availableSlots.get(index);
 			chosenSlot.setValue(Math.random() < 0.9 ? 2 : 4);
 		}
@@ -49,7 +48,9 @@ public class GameLogic {
 					while (counter != 0) {
 						comparison = gameTiles[row][counter - 1];
 						comparisonVal = comparison.getValue();
-						merged = checkForEquality();
+						if ((col - counter) == 0) {
+							merged = checkForEquality();
+						}
 						if (merged) {
 							score += current.getValue();
 							break;
@@ -90,7 +91,9 @@ public class GameLogic {
 					while (counter != 3) {
 						comparison = gameTiles[row][counter + 1];
 						comparisonVal = comparison.getValue();
-						merged = checkForEquality();
+						if ((col - counter) == 0) {
+							merged = checkForEquality();
+						}
 						if (merged) {
 							score += current.getValue();
 							break;
@@ -132,7 +135,9 @@ public class GameLogic {
 					while (counter != 0) {
 						comparison = gameTiles[counter - 1][row];
 						comparisonVal = comparison.getValue();
-						merged = checkForEquality();
+						if ((col - counter) == 0) {
+							merged = checkForEquality();
+						}
 						if (merged) {
 							score += current.getValue();
 							break;
@@ -173,7 +178,9 @@ public class GameLogic {
 					while (counter != 3) {
 						comparison = gameTiles[counter + 1][row];
 						comparisonVal = comparison.getValue();
-						merged = checkForEquality();
+						if ((col - counter) == 0) {
+							merged = checkForEquality();
+						}
 						if (merged) {
 							score += current.getValue();
 							break;
@@ -241,14 +248,10 @@ public class GameLogic {
 	public boolean gameOver() {
 		for (int i = 0; i < gameTiles.length; i++) {
 			for (int j = 0; j < gameTiles[i].length; j++) {
-				if (j != 3
-						&& gameTiles[i][j].getValue() == gameTiles[i][j + 1]
-								.getValue()) {
+				if ((j != 3) && (gameTiles[i][j].getValue() == gameTiles[i][j + 1].getValue())) {
 					return false;
 				}
-				if (i != 3
-						&& gameTiles[i][j].getValue() == gameTiles[i + 1][j]
-								.getValue()) {
+				if ((i != 3) && (gameTiles[i][j].getValue() == gameTiles[i + 1][j].getValue())) {
 					return false;
 				}
 			}
